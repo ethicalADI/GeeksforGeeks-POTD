8c6337afea19fa3973ec491bc6367541ded63810
Aditya Kumar <117146536+ethicalADI@users.noreply.github.com>
2023-07-19 10:34:51 +0530

    Create Longest_Palindromic_Subsequence.cpp

---
+++ Longest_Palindromic_Subsequence.cpp
@@ -0,0 +1,21 @@
+//Longest Palindromic Subsequence
+
+class Solution{
+  public:
+    int longestPalinSubseq(string a) {
+    string b = a;
+    int n = a.length();
+    reverse(b.begin(), b.end());
+    vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+    for(int i = 1; i < n+1; i++) {
+        for(int j = 1; j < n+1; j++) {
+            if(a[i-1] == b[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][n];
+}
+};
