d0ab06d945d128b685e0931775ae056bb0c8290a
Aditya Kumar <117146536+ethicalADI@users.noreply.github.com>
2023-07-22 13:11:10 +0530

    POTD-22/07/2023

--- Remove_duplicates_from_an_unsorted_linked_list.cpp
+++ Remove_duplicates_from_an_unsorted_linked_list.cpp
@@ -1,32 +1,22 @@
 //Remove duplicates from an unsorted linked list
 
-class Solution {
-public:
 
-double dp[25][25][104];
-int xDir[8]={ 2, 1, -1, -2, -2, -1, 1, 2 };
-int yDir[8]={ 1, 2, 2, 1, -1, -2, -2, -1 };
-
-double solveDp(int i,int j,int k, int n)
+class Solution
 {
-    if(i < 0 || j < 0 ||i >= n || j >= n) return 0;
-    if(k == 0) return 1;
-    if(dp[i][j][k] != 0) return dp[i][j][k];
-
-    double res = 0;
-
-    for(int p = 0;p < 8; ++p)
+    public:
+    //Function to remove duplicates from unsorted linked list.
+   Node * removeDuplicates( Node *head) 
     {
-      int x = i + xDir[p];
-      int y = j + yDir[p];
-      res += solveDp(x,y,k-1,n);
-    }
-    return dp[i][j][k] += (res/8.0);
-}
-
-    double knightProbability(int n, int k, int row, int column) {
-
-        memset(dp,0,sizeof(dp));
-        return solveDp(row,column,k,n); 
+        unordered_map<int, int> m;
+        Node * t = head, * p =NULL;
+        while(t){
+            if(m[t -> data]) 
+                p -> next = t -> next;
+            else 
+                p = t;
+            m[t -> data] = 1;
+            t = t -> next;
+        }
+        return head;
     }
 };
