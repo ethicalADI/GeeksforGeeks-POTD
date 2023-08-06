20fcce13f9ac2f0b7b45496269cdb4e1a71cf0a1
Aditya Kumar <117146536+ethicalADI@users.noreply.github.com>
2023-08-07 00:00:00 +0530

    POTD-06/08/2023

---
+++ String Permutations.cpp
@@ -0,0 +1,24 @@
+//String Permutations
+void solve(int i, int n, vector<string>&ans, string &S){
+        if(i==n){
+            ans.push_back(S);
+            return;
+        }
+        
+        for(int j=i; j<n; j++){
+            swap(S[i], S[j]);
+            solve(i+1, n, ans, S);
+            swap(S[i], S[j]);
+        }
+    }
+    
+    vector<string> permutation(string S)
+    {
+        //Your code here
+        int n=S.size();
+        if(n==0 || n==1) return {S}; 
+        vector<string>ans;
+        solve(0, n, ans, S);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
