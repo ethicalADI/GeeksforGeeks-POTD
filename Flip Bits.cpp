8adedaf059828bf0c15bf4086b26319e789d7372
Aditya Kumar <117146536+ethicalADI@users.noreply.github.com>
2023-08-15 16:58:18 +0530

    POTD-15/08/2023

---
+++ Flip Bits.cpp
@@ -0,0 +1,25 @@
+//Flip Bits
+
+
+class Solution{
+    public:
+    int maxOnes(int a[], int n)
+    {
+       int max=0,count=0;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(a[i]==0)
+            {count++;
+                if( max < count)
+                { max = count;
+                }
+            }else{
+                ans++;
+                if(count>0)
+                    count--;
+            }
+        }
+        return ans+max;
+    
+    }
+};
